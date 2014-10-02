b5375f413efa7d2ea7ab7c2fb6d9fdba1429bcae
tiffanym <huntergirl2015@gmail.com>
2014-10-02 12:03:15 -0400

    Update Stars.java
    Changes done in class on Oct 2, 2014

--- Stars.java
+++ Stars.java
@@ -1,18 +1,22 @@
 public class Stars{
-
-    public static String rect(int height, int width){
-	String s="";
-	int i=0;
-	while (i<height){
-	    int across=0;
-	    while (across<width){
-		s=s+"*";
-		across+=1;
-	    }
-	    s+="\n";
-	    i+=1;
+	
+	public static String mult (String s, int n){
+		String ans="";
+		while (n>0){
+			ans+=s;
+			n--;
+		}
+		return ans;
 	}
-	return s;
+	
+    public static String rect(int height, int width){
+		String s="";
+		int i=0;
+		while (i<height){
+	    	str+= mult("*",width)+"\n";
+	    	i++;
+		}
+		return s;
     }
 
     public static void main(String[]args){
