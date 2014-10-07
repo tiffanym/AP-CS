84556a91b176bb9e85b4e1fe3e1a0c7db60e0ecb
tiffanym <huntergirl2015@gmail.com>
2014-10-07 12:01:42 -0400

    Create Adventurer.java

---
+++ Adventurer.java
@@ -0,0 +1,33 @@
+public class Adventurer{
+
+    private String name;
+
+    private int HP,XP;
+
+    \\ variables must be private   
+
+    public Adventurer(){
+
+        name=”Lester”;
+
+        HP=20;
+
+        XP=0;
+
+}
+
+public String getName(){
+
+        return name;   
+
+}   
+
+public void setName(String name){
+
+        this.name=name;  
+
+    \\ since variables are private, accessors and mutators are required
+
+    }
+
+}
