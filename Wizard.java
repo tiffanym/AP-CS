dbbb06adc16b172ae1aad26f088b956a48885133
tiffanym <huntergirl2015@gmail.com>
2014-10-08 18:52:59 -0400

    Create Wizard.java

---
+++ Wizard.java
@@ -0,0 +1,15 @@
+public class Wizard extends Adventurer{
+    private int mana;
+
+    public Wizard(){
+	super();
+    }
+    
+
+    public int getmana(){
+	return mana;
+    }
+    public void setmana(int mana){
+	this.mana=mana;
+    }
+}
