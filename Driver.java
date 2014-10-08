0d32b586f414ad60ee866ff72197fd67f4c04497
tiffanym <huntergirl2015@gmail.com>
2014-10-08 18:53:30 -0400

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,18 @@
+public class Driver{
+    public static void main(String[]args){
+	/*
+	Adventurer a= new Adventurer("Bob");
+	Warrior b= new Warrior("Dan",15);
+	Wizard c= new Wizard("Bob",42);
+	Adventurer d= new Warrior("Dave");
+	
+	System.out.println("Name: "+a.getName()+" HP: "+a.getHP());
+	System.out.println("Name: "+b.getName()+" HP: "+b.getHP());
+	System.out.println("Name: "+c.getName()+" Mana: "+c.getmana());
+	System.out.println("Name: "+d.getName()+" HP: "+d.getHP());
+	*/
+	Adventurer p1=new Adventurer("Bob");
+	Warrior p2=new Warrior();
+	System.out.println(p2.getHP());
+    }
+}
