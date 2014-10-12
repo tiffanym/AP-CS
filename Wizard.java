aec01fb4c7d4e08487694a55d72e4eacbcbe5c9d
tiffanym <huntergirl2015@gmail.com>
2014-10-11 21:03:07 -0400

    Update Wizard.java
    Created specialAttack() method

--- Wizard.java
+++ Wizard.java
@@ -1,15 +1,24 @@
 public class Wizard extends Adventurer{
     private int mana;
-
+    
+    //constructors
+    public Wizard(String name){
+	super(name);
+    }
     public Wizard(){
 	super();
     }
     
-
+    //get and set methods for variables
     public int getmana(){
 	return mana;
     }
     public void setmana(int mana){
 	this.mana=mana;
+    }
+    
+    //attack method(s)
+    public String specialAttack(Adventurer other){
+	return getName()+" switches staff for wand and uses mana power to critically burn "+other.getName();
     }
 }
