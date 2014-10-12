52bb3cd2adc23436a5c1b3a67f7ffa447b3abf91
tiffanym <huntergirl2015@gmail.com>
2014-10-11 21:02:00 -0400

    Update Warrior.java
    created variable rage, added set and get methods for rage variable, and created specialAttack() method

--- Warrior.java
+++ Warrior.java
@@ -1,12 +1,25 @@
 public class Warrior extends Adventurer{
-    private int bonusHealth;
+    private int rage;
     
+    //constuctors
+    public Warrior(String s){
+	super(s);
+    }
     public Warrior(){
 	super();
     }
-    
 
-    public int getHP(){
-	return super.getHP()+bonusHealth;
-    } 
-}
+    //get and set methods for variables
+    public int getrage(){
+	return rage;
+    }
+
+    public void setrage(int rage){
+	this.rage=rage;
+    }
+    
+    //attack method(s)
+    public String specialAttack(Adventurer other){
+	return getName()+" uses full rage and critically strikes "+other.getName();
+    }
+}   
