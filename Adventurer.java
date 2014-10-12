ba645ab78fcb88c2555446df488e699ec0fd2cf4
tiffanym <huntergirl2015@gmail.com>
2014-10-11 20:59:01 -0400

    Update Adventurer.java
    Added attack() method

--- Adventurer.java
+++ Adventurer.java
@@ -1,14 +1,15 @@
 public class Adventurer{
     private String name;
     private int HP;
-    
+
+    //constructors
     public Adventurer(String name){
     	setName(name);
     }
-    
     public Adventurer(){   
     }
-    
+
+    //setters and getters for variables    
     public String getName(){
 	return name;
     }
@@ -21,5 +22,13 @@ public class Adventurer{
     }
     public void setHP(int HP){
 	this.HP=HP;
+    }
+
+    //attack method
+    public String attack(Adventurer other){
+	if (getName().equals(other.getName())){
+	    return "I'm sorry, " +getName()+" but we don't promote self-mutilation in this game.";
+	}
+	return getName() + " attacks " + other.getName();
     }
 }
