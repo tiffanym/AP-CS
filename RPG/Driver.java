50b4c5d7bdfcd169b457c2512487a4063f9897e8
tiffanym <huntergirl2015@gmail.com>
2014-10-14 22:13:43 -0400

    Update Driver.java
    added things to test out if hit and attack worked

--- RPG/Driver.java
+++ RPG/Driver.java
@@ -11,9 +11,16 @@ public class Driver{
 	System.out.println(d.attack(a));
 	System.out.println("What happens if Bob attacks himself?" +
 			   a.attack(a));
-	*/
+	
 	System.out.println(b.specialAttack(a));
 	System.out.println(c.specialAttack(a));
 	System.out.println(d.specialAttack(a));
+	*/
+	System.out.println(a.toString());
+	System.out.println(b.toString());
+	System.out.println(a.hit(b,.7));
+	System.out.println(b.hit(a,.5));
+	System.out.println(a.attack(b));
+	System.out.println(b.attack(a));
     }
 }
