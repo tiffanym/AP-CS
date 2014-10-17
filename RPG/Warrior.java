48ddca1ffc709c85ab923a5179f9567f31dc3494
tiffanym <huntergirl2015@gmail.com>
2014-10-16 22:43:33 -0400

    Update Warrior.java
    edited specialAttack() and created calculations in constructor so that STR would be the highest of all types (Warrior, Wizard, Rogue, Adventurer), DEX would be the second highest, INT would be the worst/ lowest

--- RPG/Warrior.java
+++ RPG/Warrior.java
@@ -4,6 +4,10 @@ public class Warrior extends Adventurer{
     //constuctors
     public Warrior(String s){
 	super(s);
+	setrage((int)(Math.random()*15)+1);
+	setSTR((int)(Math.random()*51)+10);
+	setDEX((int)(Math.random()*41)+10);
+	setINT((int)(Math.random()*21)+10);
     }
     public Warrior(){
 	super();
@@ -17,9 +21,22 @@ public class Warrior extends Adventurer{
     public void setrage(int rage){
 	this.rage=rage;
     }
-    
-    //attack method(s)
+   
+    //toString method
+    public String toString(){
+	return super.toString()+" "+getrage()+" rage";
+    }
+
+    //attack method(s)    
     public String specialAttack(Adventurer other){
-	return getName()+" uses full rage and critically strikes "+other.getName();
+	double hitchance= (double)(Math.random()*2)+0;
+	if (hit (other,hitchance) && getrage()-2>=0){
+	    int x=(int)((1.00- (double)(getDEX()/other.getDEX()))*1.5)*getHP();
+	    x=x+10;
+	    setrage(getrage()-2);
+	    return getName()+" uses full rage and critically strikes "+
+		other.getName()+" and does "+x+" damage";
+	}
+	return "Not enough rage or not high enough hit chance \n"+attack(other);
     }
 }   
