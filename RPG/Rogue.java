ddd3cc3c46376491cf5d3f2b5463af8afe376e82
tiffanym <huntergirl2015@gmail.com>
2014-10-16 22:46:09 -0400

    Update Rogue.java
    edited specialAttack() and created calculations in constructor so that STR would be the lowest of all types (Warrior, Wizard, Rogue, Adventurer), DEX would be the highest, INT would be the 2nd highest

--- RPG/Rogue.java
+++ RPG/Rogue.java
@@ -4,6 +4,10 @@ public class Rogue extends Adventurer{
     //constructors
     public Rogue (String name){
 	super(name);
+	setSTA((int)(Math.random()*20)+1);
+	setSTR((int)(Math.random()*21)+10);
+	setDEX((int)(Math.random()*51)+10);
+	setINT((int)(Math.random()*41)+10);
     }
     public Rogue(){
 	super();
@@ -17,9 +21,21 @@ public class Rogue extends Adventurer{
 	this.stamina=stamina;
     }
 
+    //toString method
+    public String toString(){
+	return super.toString()+" "+getSTA()+" stamina";
+    }
+
     //attack method(s)
     public String specialAttack(Adventurer other){
-	return getName()+" stealthily hides in the trees and throws shurikens at "+other.getName();
+	double hitchance= (double)(Math.random()*2)+0;
+	if (hit (other,hitchance) && getSTA()-2>=0){
+	    int x=(int)((1.00- (double)(getDEX()/other.getDEX()))*1.3)*getHP();
+	    x=x+5;
+	    setSTA(getSTA()-2);
+	    return getName()+" stealthily hides in the trees and throws shurikens at "+other.getName()
+		+" and does "+x+" damage";
+	}
+	return "Not enough stamina or not high enought hit chance \n"+attack(other);
     }
-    
 }
