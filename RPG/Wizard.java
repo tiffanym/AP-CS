c72ae2c0398f8cfd612459890c9ca2f515bdc8a6
tiffanym <huntergirl2015@gmail.com>
2014-10-16 22:45:08 -0400

    Update Wizard.java
    edited specialAttack() and created calculations in constructor so that STR would be the 2nd highest of all types (Warrior, Wizard, Rogue, Adventurer), DEX would be the lowest, INT would be the highest

--- RPG/Wizard.java
+++ RPG/Wizard.java
@@ -4,6 +4,10 @@ public class Wizard extends Adventurer{
     //constructors
     public Wizard(String name){
 	super(name);
+	setmana((int)(Math.random()*10)+1);
+	setSTR((int)(Math.random()*41)+10);
+	setDEX((int)(Math.random()*21)+10);
+	setINT((int)(Math.random()*51)+10);
     }
     public Wizard(){
 	super();
@@ -16,9 +20,31 @@ public class Wizard extends Adventurer{
     public void setmana(int mana){
 	this.mana=mana;
     }
-    
+
+    //toString method
+    public String toString(){
+	return super.toString()+" "+getmana()+" mana";
+    }
+   
+    //hitMagic() method for wizard to calculate hit%
+    public boolean hitMagic(Adventurer other, double hitchance){
+	double actualhitrate=this.getINT()/other.getINT();
+	if (actualhitrate<=hitchance && Math.abs(actualhitrate)<=1){
+	    return true;
+	}
+	else{
+	    return false;
+	}
+    }
+
     //attack method(s)
     public String specialAttack(Adventurer other){
-	return getName()+" switches staff for wand and uses mana power to critically burn "+other.getName();
+	double hitchance= (double)(Math.random()*2)+0;
+	if (hitMagic (other,hitchance) && getmana()-2>=0){
+	    int x=(int)((1.00- (double)(getDEX()/other.getDEX()))*1.1)*getHP();
+	    x=x+2;
+	    return getName()+" switches staff for wand and uses mana power to critically burn "+other.getName()+" and does "+x+" damage";	  
+	}
+	return "Not enough mana or not high enough hit chance \n"+attack(other);
     }
 }
