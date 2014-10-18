9933712215a761d647f549dfa8cecc5acf22027e
tiffanym <huntergirl2015@gmail.com>
2014-10-18 16:09:30 -0400

    Update Game.java
    Added: check that if input isn't valid, will keep repeating message until input is.
    Problems: if invalid input at least once, when put valid input later on, the "You picked ..." message doesn't come up

--- RPG/Game.java
+++ RPG/Game.java
@@ -1,11 +1,30 @@
 import java.util.Scanner;
 public class Game{
+    Adventurer player,oppenent;
     public static void main(String[]args){
-	Adventurer player=new Adventurer();
-	Adventurer other=new Adventurer();
-
+	
 	Scanner scan=new Scanner(System.in);
-	System.out.prinln("Choose a class: \n A : Warrior \n B : Wizard \n"+                           "C : Rogue");
-	String playerclass=scan.nextLine()
+	System.out.println("Choose a class: \n A : Warrior \n B : Wizard \n"+"C : Rogue");
+	String playerclass=scan.nextLine().toUpperCase();
+	
+	while ("ABC".indexOf(playerclass.toUpperCase())==-1){
+		System.out.println("ERROR Please enter a valid option.");
+		System.out.println();
+		System.out.println("Choose a class: \n"+"A : Warrior \n"+"B : Wizard \n"+"C : Rogue");
+		playerclass = scan.nextLine();
+	}
+	if (playerclass.equals("A")){
+	    System.out.println("You picked Warrior.");
+	    Warrior player=new Warrior();
+	}
+	else if (playerclass.equals("B")){
+	    System.out.println("You picked Wizard.");
+	    Wizard player=new Wizard();
+	}
+	else if (playerclass.equals("C")){
+	    System.out.println("You picked Rogue.");
+	    Rogue player=new Rogue();
+	}
+	Adventurer opponent=new Adventurer();
     }
 }
