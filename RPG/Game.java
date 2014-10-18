98bd76bfb010ee3b5b605272cdac07fc442969e3
tiffanym <huntergirl2015@gmail.com>
2014-10-18 14:26:37 -0400

    Create Game.java

---
+++ RPG/Game.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Game{
+    public static void main(String[]args){
+	Adventurer player=new Adventurer();
+	Adventurer other=new Adventurer();
+
+	Scanner scan=new Scanner(System.in);
+	System.out.prinln("Choose a class: \n A : Warrior \n B : Wizard \n"+                           "C : Rogue");
+	String playerclass=scan.nextLine()
+    }
+}
