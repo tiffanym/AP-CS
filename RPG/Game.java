ace136c6276fdb143759f950c6f5e655e8a92448
tiffanym <huntergirl2015@gmail.com>
2014-10-18 21:54:16 -0400

    Update Game.java
    added a bunch of stuff for AP CS HW:
    Homework Due Sunday 11:59pm on the homework server! (zip all of your JAVA files together)
    Step 1.
    Make sure your Adventurer, Warrior, Wizard, and Rogue work using my driver (included at end of post)
    
    Step 2.
    Learn the Scanner class.
    http://docs.oracle.com/javase/7/docs/api/java/util/Scanner.html
    
    Example:
    Scanner in = new Scanner(System.in);
    System.out.println("Type something:");
    String line = in.nextLine();
    System.out.println("You typed: <"+line+"> without the <>");
        
    
    Step 3.
    
    3.1 Make a new driver: Game.java
    
    3.2 Create variables for the player controlled character, and their opponent:
    Adventurer player,opponent;
    
    3.3 Prompt the user which class they would like to be:
    Choose a class:
    A : Warrior
    B : Wizard
    C : Rogue
    
    3.4 Read a value from the scanner then initialize player with the appropriate Adventurer type. If the user types an invalid choice, ask them to type it again.
    
    3.5 Initialize the opponent to be a regular Adventurer.
    
    3.6 Now allow the player to combat the opponent. 
    You can decide how to figure out who goes first, the player or the opponent.
    You can decide how the opponent attacks the player.
    
    3.6.1
    You must repeatedly get input and decide what to do, each round 
    a) Print the player + opponent stats.
    b) prompt the user:
    Choose an action:
    A : attack
    S : special attack
    G : give up
    
    You would then want to read what the user types, and use the appropriate attack, or end the match.
    
    3.6.2
    You must decide when to end the match. When either the player or opponent have less than 1 HP, OR the player gives up, you should end the match, and display information about the results.
    
    
    3.7 Improve your game by adding some extra features: (this will save you time later)
    3.7.1
    -Allow the opponent to be randomly chosen as Warrior/Wizard/Rogue/Adventurer.
    -Allow the opponents to use their special attacks ONLY when they have enough resources. Adventurers have no special resource, therefore should randomly choose.
    3.7.2
    -Allow the user to create their character by allocating 30 stat points between their STR/DEX/INT.
    -This requires that you learn to convert the user input into integers. Look at   Integer.parseInt()  OR   Scanner.nextInt()

--- RPG/Game.java
+++ RPG/Game.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 public class Game{
-    Adventurer player,oppenent;
+
     public static void main(String[]args){
-	
+	Adventurer player,opponent;	
+	//user picks their class/ Adventurer type
 	Scanner scan=new Scanner(System.in);
 	System.out.println("Choose a class: \n A : Warrior \n B : Wizard \n"+"C : Rogue");
 	String playerclass=scan.nextLine().toUpperCase();
@@ -14,17 +15,211 @@ public class Game{
 		playerclass = scan.nextLine();
 	}
 	if (playerclass.equals("A")){
-	    System.out.println("You picked Warrior.");
-	    Warrior player=new Warrior();
+	    System.out.println("You picked Warrior.\n");
+	    player=new Warrior();
+	    //(Warrior)player;
 	}
 	else if (playerclass.equals("B")){
-	    System.out.println("You picked Wizard.");
-	    Wizard player=new Wizard();
+	    System.out.println("You picked Wizard.\n");
+	    player=new Wizard();
+	    //(Wizard)player;
 	}
 	else if (playerclass.equals("C")){
-	    System.out.println("You picked Rogue.");
-	    Rogue player=new Rogue();
+	    System.out.println("You picked Rogue.\n");
+	    player=new Rogue();
+	    //(Rogue)player;
 	}
-	Adventurer opponent=new Adventurer();
+	else{
+	    player=new Adventurer();
+	}
+
+	//opponent randomly chosen as Warrior/Wizard/Rogue/Adventurer
+	int opponentnumber=(int)(Math.random()*4)+1;
+	if (opponentnumber==1){
+	    opponent=new Warrior("Maximus, the Evil Thing");
+	}
+	else if(opponentnumber==2){
+	    opponent=new Wizard("Merlindine, the Evil Thing");
+	}
+	else if(opponentnumber==3){
+	    opponent=new Rogue("Roobiedaloo, the Evil Thing");
+	}
+	else{
+	    opponent=new Adventurer("Some Guy on the Street, the Evil Thing");
+	}
+	
+	//user picks their name
+	Scanner namescan=new Scanner(System.in);
+	System.out.println("What is your name?");
+	String name=namescan.nextLine();
+	player.setName(name);
+	System.out.println("*Salutes* I welcome you "+name+".\n");
+
+	//player can "create" character y allocating 30 stat points b/w their STR/DEX/INT
+	System.out.println("Here are your current stats:");
+	System.out.println(player.getStats());
+	System.out.println();
+	System.out.println("You have 30 stat points to distribute b/w STR/DEX/INT");
+	int x=30;
+	//adds to STR stat
+	Scanner allocateSTR=new Scanner(System.in);
+	System.out.println("How many points would you like to add to STR?");
+	int addSTR=allocateSTR.nextInt();
+	while (addSTR>x || addSTR<0){
+	    System.out.println("ERROR Please enter a number between 0 and 30");
+	    System.out.println("How many point would you like to add to STR?");
+	    addSTR=allocateSTR.nextInt();
+	}
+	x=x-addSTR;
+	player.setSTR(player.getSTR()+addSTR);
+	System.out.println("You have added "+addSTR+" stat points to STR. \n"+" Your strength is now "+player.getSTR()+".");
+	if (x>0){
+	    //adds to DEX stat
+	    System.out.println("You have "+x+" stat points to distribute b/w DEX and INT");
+	    Scanner allocateDEX=new Scanner(System.in);
+	    System.out.println("How many points would you like to add to DEX?");
+	    int addDEX=allocateDEX.nextInt();
+	    while (addSTR>=x || addSTR<0){
+		System.out.println("ERROR Please enter a number between 0 and "+x);
+		System.out.println("How many point would you like to add to DEX?");
+		addDEX=allocateDEX.nextInt();		
+	    }
+	    x=x-addDEX;
+	    player.setDEX(player.getDEX()+addDEX);
+	    System.out.println("You have added "+addDEX+" stat points to DEX. \n"+" Your dexterity is now "+player.getDEX()+".");	    
+	    if (x>0){
+		//adds to INT
+		System.out.println("You have "+x+ "stat points to distribute to INT");
+		Scanner resttoINTq=new Scanner(System.in);
+		//asks if wants to add all points left to INT
+		System.out.println("Would you like to distribute your remaining points to INT? [Y/N]");
+		String resttoINTa=resttoINTq.nextLine().toUpperCase();
+		//adds all leftover points to INT
+		if (resttoINTa.equals("Y")){
+		    player.setINT(player.getINT()+x);
+		    System.out.println("You have added "+x+ " stat points to INT. \n"+"Your intelligence is now "+player.getINT()+".");
+		    x=0;
+		}
+		//adds certain number of leftover points to INT
+		else if (resttoINTa.equals("N")){
+		    Scanner allocateINT=new Scanner(System.in);
+		    System.out.println("How many points would you like to add to INT?");
+		    int addINT=allocateINT.nextInt();
+		    while (addINT>=x || addINT<0){
+			System.out.println("ERROR Please enter a number between 0 and "+x);
+			System.out.println("How many point would you like to add to INT?");
+			addINT=allocateINT.nextInt();		
+		    }
+		    x=x-addINT;
+		    player.setINT(player.getINT()+addINT);
+		    System.out.println("You have added "+addINT+" stat points to INT. \n"+"Your intelligence is now "+player.getINT()+".");
+		    System.out.println();
+		    //checking to make sure player wants to add any leftover points or leave it as is
+		    Scanner whattodowithrest=new Scanner(System.in);
+		    System.out.println("Are you sure you don't want to allocate the rest of your points to one of the stats? \n"+
+				       "You still have "+x+" stat points left to use. \n"+
+				       "Whaddya say? \n"+
+				       "A : Add the rest of my points to STR \n"+
+				       "B : Add the rest of my points to DEX \n"+
+				       "C : Add the rest of my points to INT \n"+
+				       "D : LEAVE ME ALONE AND LET ME PLAY THE GAME!!!");
+		    String response=whattodowithrest.nextLine().toUpperCase();
+		    while ("ABCD".indexOf(response.toUpperCase())==-1){
+			System.out.println("ERROR Please enter a valid option.");
+			System.out.println();
+			System.out.println("Choose one of the options below: \n"+
+					   "A : Add the rest of my points to STR \n"+
+					   "B : Add the rest of my points to DEX \n"+
+					   "C : Add the rest of my points to INT \n"+
+					   "D : LEAVE ME ALONE AND LET ME PLAY THE GAME!!!");
+			response = whattodowithrest.nextLine();
+		    }
+		    if (response.equals("A")){
+			int newAddSTR=x;
+			player.setSTR(player.getSTR()+newAddSTR);
+			System.out.println("You have added "+newAddSTR+" stat points to STR. \n"+" Your strength is now "+player.getSTR()+".");
+		    }
+		    else if (response.equals("B")){
+			int newAddDEX=x;
+			player.setDEX(player.getDEX()+newAddDEX);
+			System.out.println("You have added "+newAddDEX+" stat points to DEX. \n"+" Your dexterity is now "+player.getDEX()+".");
+		    }
+		    else if (response.equals("A")){
+			int newAddINT=x;
+			player.setINT(player.getINT()+newAddINT);
+			System.out.println("You have added "+newAddINT+" stat points to INT. \n"+" Your intelligence is now "+player.getINT()+".");
+		    }
+		    else if (response.equals("A")){
+			System.out.println("Okay, okay, geez."+ "\n *Sooomeone's got anger issues O_O*");
+		    }
+		}
+	    }
+	}
+	else{
+	    System.out.println("You have allocated all your stat points to STR, and so none are left for DEX or INT. :'(");
+	}
+	System.out.println("Here are your updated stats:");
+	System.out.println(player.getStats());
+	System.out.println();	
+
+	//Player and Opponent now in combat mode
+	System.out.println("You, "+name+", will be going against "+opponent.getName()+". \n");
+	//checks readiness/ "Are you still alive right now"-ness
+	Scanner readyscan=new Scanner(System.in);
+	System.out.println("Are you ready? [Y/N]");
+	String ready=readyscan.nextLine().toUpperCase();
+	if (ready.equals("Y")){
+	    System.out.println("Awesome! Let's get started: \n");
+	}
+	else if(ready.equals("N")){
+	    System.out.println("Too bad. You signed up for this, so stop complaining. >:-) \n");
+	}
+
+	System.out.println("Out of courtesy, opponent will allow "+player.getName() +" to go first. \n");
+	//lists stats
+	while (player.getHP()>0 && opponent.getHP()>0){
+	    System.out.println(player.getStats());
+	    System.out.println(opponent.getStats());
+	    System.out.println();
+	    //player attacks
+	    Scanner actionscan=new Scanner(System.in);
+	    System.out.println("Choose an action: \n"+"A : attack \n"+"B : special attack \n"+"C : give up");
+	    String action=actionscan.nextLine().toUpperCase();
+	    while ("ABC".indexOf(action.toUpperCase())==-1){
+		System.out.println("ERROR Please enter a valid option.");
+		System.out.println();
+		System.out.println("Choose an action: \n"+"A : attack \n"+"B : special attack \n"+"C : give up");
+		action = actionscan.nextLine();
+	    }
+	    if (action.equals("A")){
+		System.out.println(player.attack(opponent));
+	    }
+	    else if (action.equals("B")){
+		System.out.println(player.specialAttack(opponent));
+	    }
+	    else if (action.equals("C")){
+	    System.out.println("How lazy. What kind of true player gives up? Fine, I'll let it go this time.");
+	    }
+	    //opponent attacks
+	    String oppclass=opponent.getClass().getSimpleName();
+	    
+	    /*
+	    int x=(int)(Math.random()*5)+1;
+	    if (x==1){
+		System.out.println(opponent.attack(player)+"\n");
+	    }
+	    else{
+		System.out.println(opponent.specialAttack(player)+"\n");
+	    }
+	    */
+	}
+	//announcing results
+	if (opponent.getHP()<=0){
+	    System.out.println("Yay! "+player.getName()+" won!!!");
+	}
+	else if (player.getHP()<=0){
+	    System.out.println("Aw, "+player.getName()+" lost. :'("+ "\n"+"Well boo-hoo for you, but somebody (a.k.a. "+opponent.getName()+") is going home a winner!!!! \n"+"I mean.... uh... everyone's a winner? ^v^;");
+	}
+
     }
 }
