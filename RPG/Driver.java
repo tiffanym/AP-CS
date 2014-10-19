c4cd6d23aafde452a2cfeca8a002a1926fc9a3be
tiffanym <huntergirl2015@gmail.com>
2014-10-18 21:55:49 -0400

    Update Driver.java
    the not-starred out text (the one that's actually going to run and compile) is [most] of Sample Driver posted under 2014-10-17 HW

--- RPG/Driver.java
+++ RPG/Driver.java
@@ -1,27 +1,64 @@
 public class Driver{
     public static void main(String[]args){
-	Adventurer a= new Adventurer("Bob");
-	Warrior b = new Warrior("Dan");
+	Adventurer p1 =new Adventurer("Chadwick");
+	Warrior p2 =new Warrior("Grobnar");
+	Wizard p3 = new Wizard("Merlin");
+	Rogue p4 = new Rogue("Inigo Montoya");
+	
+	System.out.println(p1.getStats());
+	System.out.println(p2.getStats());
+	System.out.println(p3.getStats());
+	System.out.println(p4.getStats());
+	System.out.println();
+	
+	System.out.println(p1.attack(p2));
+	System.out.println(p1.specialAttack(p2));
+	System.out.println();
+	
+	System.out.println(p2.attack(p3));
+	System.out.println(p2.specialAttack(p3));
+	System.out.println();
+	
+	System.out.println(p3.attack(p4));
+	System.out.println(p3.specialAttack(p4));
+	System.out.println();
+	
+	System.out.println(p4.attack(p1));
+	System.out.println(p4.specialAttack(p1));
+	System.out.println();
+	
+	System.out.println(p1.getStats());
+	System.out.println(p2.getStats());
+	System.out.println(p3.getStats());
+	System.out.println(p4.getStats());
+	System.out.println();
+    }
+}
+/*
+public class Driver{
+    public static void main(String[]args){
+	Adventurer a= new Adventurer("Chadwick");
+	Warrior b = new Warrior("Grobnar");
 	Wizard c= new Wizard("Merlin");
-	Rogue d= new Rogue("Al");
-
+	Rogue d= new Rogue("Inigo Montoya");
+	
 	System.out.println(a.getStats());
 	System.out.println(b.getStats());
 	System.out.println(c.getStats());
 	System.out.println(d.getStats());
-	System.out.println();
+	//System.out.println(a.hit(b,.7));
+	//System.out.println(b.hit(a,.5));
 
+	System.out.println();
 	System.out.println(a.attack(b));
 	System.out.println();
 
 	System.out.println(b.attack(c));
 	System.out.println(b.specialAttack(a));
 	System.out.println();
-
 	System.out.println(c.attack(d));
 	System.out.println(c.specialAttack(b));
 	System.out.println();
-
 	System.out.println(d.attack(a));
 	System.out.println(d.specialAttack(c));
 	System.out.println();
@@ -30,5 +67,7 @@ public class Driver{
 	System.out.println(b.getStats());
 	System.out.println(c.getStats());
 	System.out.println(d.getStats());
+
     }
 }
+*/
