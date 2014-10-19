221a4ec5d2667c9169e83d8612349a84a5410306
tiffanym <huntergirl2015@gmail.com>
2014-10-18 21:58:01 -0400

    Update Rogue.java

--- RPG/Rogue.java
+++ RPG/Rogue.java
@@ -11,6 +11,7 @@ public class Rogue extends Adventurer{
     }
     public Rogue(){
 	super();
+	setSTA((int)(Math.random()*20)+1);
     }
 
     //get and set methods for variables
@@ -29,13 +30,18 @@ public class Rogue extends Adventurer{
     //attack method(s)
     public String specialAttack(Adventurer other){
 	double hitchance= (double)(Math.random()*2)+0;
-	if (hit (other,hitchance) && getSTA()-2>=0){
-	    int x=(int)((1.00- (double)(getDEX()/other.getDEX()))*1.3)*getHP();
-	    x=x+5;
+	String msg=getName()+" special attacks "+other.getName()+"\n";
+	if (hit (other,hitchance) && getSTA()-2>=0 && getHP()>0){
+	    int x=(int)((1.00- (double)(getDEX()/other.getDEX()))*1.3)*getDEX();
+	    x=Math.abs(x)+5;
 	    setSTA(getSTA()-2);
-	    return getName()+" stealthily hides in the trees and throws shurikens at "+other.getName()
+	    other.setHP(other.getHP()-x);
+	    return msg+="=>"+getName()+" stealthily hides in the trees and throws shurikens at "+other.getName()
 		+" and does "+x+" damage";
 	}
-	return "Not enough stamina or not high enought hit chance \n"+attack(other);
+	else if(getHP()<=0){
+	    return msg+="=>Not enough HP. Sorry, "+getName()+", can't attack when you're dead";
+	}
+	return msg+="=>Not enough stamina or not high enought hit chance to use special attack \n"+"==>"+getName()+" will attack normally instead: \n"+attack(other);
     }
 }
