b79572d03ccf906be3b2131dfaa60adbb4eee37e
tiffanym <huntergirl2015@gmail.com>
2014-10-20 18:29:31 -0400

    Update Driver.java
    Changed it so it would match with the fact that Adventurer.java became abstract

--- RPG/Driver.java
+++ RPG/Driver.java
@@ -1,19 +1,19 @@
 public class Driver{
     public static void main(String[]args){
-	Adventurer p1 =new Adventurer("Chadwick");
+	//Adventurer p1 =new Adventurer("Chadwick");
 	Warrior p2 =new Warrior("Grobnar");
 	Wizard p3 = new Wizard("Merlin");
 	Rogue p4 = new Rogue("Inigo Montoya");
 	
-	System.out.println(p1.getStats());
+	//System.out.println(p1.getStats());
 	System.out.println(p2.getStats());
 	System.out.println(p3.getStats());
 	System.out.println(p4.getStats());
 	System.out.println();
 	
-	System.out.println(p1.attack(p2));
-	System.out.println(p1.specialAttack(p2));
-	System.out.println();
+	//System.out.println(p1.attack(p2));
+	//System.out.println(p1.specialAttack(p2));
+	//System.out.println();
 	
 	System.out.println(p2.attack(p3));
 	System.out.println(p2.specialAttack(p3));
@@ -23,11 +23,11 @@ public class Driver{
 	System.out.println(p3.specialAttack(p4));
 	System.out.println();
 	
-	System.out.println(p4.attack(p1));
-	System.out.println(p4.specialAttack(p1));
-	System.out.println();
+	//System.out.println(p4.attack(p1));
+	//System.out.println(p4.specialAttack(p1));
+	//System.out.println();
 	
-	System.out.println(p1.getStats());
+	//System.out.println(p1.getStats());
 	System.out.println(p2.getStats());
 	System.out.println(p3.getStats());
 	System.out.println(p4.getStats());
