45766ab392321d4dc06f1fcce1c0facea5bc4944
tiffanym <huntergirl2015@gmail.com>
2014-10-30 23:32:20 -0500

    Update MartialArtist.java

--- RPG/MartialArtist.java
+++ RPG/MartialArtist.java
@@ -25,11 +25,52 @@ public class MartialArtist extends Adventurer{
 	return getName()+" ("+getClass().getSimpleName()+")"+"\n"+getHP()+"HP  "+getSTR()+"STR  "+getDEX()+"DEX  "+getINT()+"INT "+getqui()+" qui";
     }
 
+    //because Martial Artist is just that awesome
+    public int highestStat(){
+	int highest=getSTR();
+	if (getDEX()>highest){
+	    highest=getDEX();
+	}
+	else if (getINT()>highest){
+	    highest=getINT();
+	}
+	return highest;	
+    }
+
     //attack method(s)    
+    public String attack(Adventurer other){
+	String msg=getName()+" the "+getClass().getSimpleName()+" attacks "+other.getName()+" the "+other.getClass().getSimpleName()+"\n";
+	if (hit(other) && getHP()>0 && other.getHP()>0){
+	    int x=(int)(this.highestStat()*2/3);
+	    x=Math.abs(x);
+	    if (other.getHP()-x>0 && x>0){	       
+		other.setHP(other.getHP()-x);
+		msg=msg+"=>Yay! "+getName()+" successfully hit "+
+		    other.getName()+" and did "+ x+ " damage";
+	    }
+	    else if (other.getHP()-x<=0 && x>0){
+		other.setHP(0);
+		msg=msg+"=>Dayum! "+getName()+" did " +x+
+		    " damage and KOed "+other.getName();
+	    }
+	}
+	else if (getHP()<=0){
+	    msg+="=>Not enough HP. Sorry,"+getName()+" can't attack when you're dead";
+	}
+	else if(other.getHP()<=0){
+	    return msg+="=>Well, um... "+ other.getName()+"is kinda... sorta... dead. x.x";
+	}	
+	else{
+	    msg= msg+"=>...and misses :(";
+	    int x=(int)(this.getSTR()*2/3);
+	}
+	return msg;
+    }
+
     public String specialAttack(Adventurer other){
 	double hitchance= (double)(Math.random()*2)+0;
 	String msg=getName()+" special attacks "+other.getName()+"\n";
-	if (hit (other,hitchance) && getqui()-2>=0 && getHP()>0){
+	if (hit (other) && getqui()-2>=0 && getHP()>0 && other.getHP()>0){
 	    int x=(int)((1.00- (double)(getDEX()/other.getDEX()))*1.5)*getDEX();
 	    x=Math.abs(x)+12;
 	    setqui(getqui()-2);
@@ -39,6 +80,9 @@ public class MartialArtist extends Adventurer{
 	}
 	else if(getHP()<=0){
 	    return msg+="=>Not enough HP. Sorry, "+getName()+", can't attack when you're dead";
+	}
+	else if(other.getHP()<=0){
+	    return msg+="=>Well, um... "+ other.getName()+"is kinda... sorta... dead. x.x";
 	}
 	return msg+="=>Not enough qui or not high enough hit chance to use special attack \n"+
 	    "==>"+getName()+" will attack normally instead: \n"+attack(other);
