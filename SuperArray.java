0de2421312dcc0470e32e5f5d053e42f7e356812
tiffanym <huntergirl2015@gmail.com>
2014-11-06 12:34:06 -0500

    Update SuperArray.java

--- SuperArray.java
+++ SuperArray.java
@@ -3,19 +3,57 @@ public class SuperArray{
     private int numElements;
 
     public SuperArray(){
-	data=new SuperArray[10];
+	numElements=10;
+	data=new SuperArray[numElements];
     }
 
     public SuperArray(int numElements){
 	this.numElements=numElements;
+	data=new SuperArray[numElements];
     }
 
     public String toString(){
 	String s="[";
-	for (int i=0;i<SuperArray.length;i++){
-	    s+=" "+(String)(SuperArray);
+	for (int i=0;i<data.length;i++){
+	    s+=" "+(String)(data[i]);
 	}
-	return s;
+	return s+"]";
     }
 
+    public void add(Object e){
+	resize(data.length+1);
+	data[data.length-1]=e;
+    }
+    
+    public int size(){
+	return data.length;
+    }
+
+    public void resize(int newSize){
+	Object[] newdata=new Object[newSize];
+	int smaller=size();
+	if (newSize<=size()){
+	    smaller=newSize;
+	}
+	for (int i=0;i<smaller;i++){
+	    newdata[i]=data[i];
+	}
+	data=newdata;
+    }
+
+    public static void main(String[]args){
+	SuperArray data=new SuperArray();
+	//SuperArray nextd=new SuperArray(5);
+	data.resize(8);
+	//nextd.resize(6);
+	data.add("happy");
+	//nextd.add("happy");
+
+	Integer x = new Integer(5);
+	SuperArray L = new SuperArray();
+	L.add(x);
+	L.add(new Integer(99));
+	System.out.println(data.toString());
+	//System.out.println(nextd.toString());
+    }
 }
